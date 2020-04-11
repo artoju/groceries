2015b6403417d6078f191fd675a4bddf0c116c18
Arto Jussilainen <artojussilainen@gmail.com>
2020-04-11 19:59:47 +0300

    added add button

--- src/GroceryAdder.tsx
+++ src/GroceryAdder.tsx
@@ -1,26 +1,60 @@
 import React from 'react';
-import TextField from '@material-ui/core/TextField';
+import InputBase from '@material-ui/core/InputBase';
+import { makeStyles } from '@material-ui/core/styles';
+import AddIcon from '@material-ui/icons/Add';
+import Paper from '@material-ui/core/Paper';
+import IconButton from '@material-ui/core/IconButton';
+import { Divider } from '@material-ui/core';
 
 type GroceryAdderProps = {
     addFn: Function
 }
 
+const useStyles = makeStyles((theme) => ({
+    root: {
+      padding: '2px 4px',
+      display: 'flex',
+      alignItems: 'center',
+    },
+    input: {
+      marginLeft: theme.spacing(1),
+      flex: 1,
+    },
+    iconButton: {
+      padding: 10,
+    },
+    divider: {
+      height: 28,
+      margin: 4,
+    },
+  }));
+
 export const GroceryAdder = ({ addFn }: GroceryAdderProps) => {
     const [name, setName] = React.useState<string>("");
-
+    const classes = useStyles()
 
 
     const handleKeyPress = (event: React.KeyboardEvent<HTMLInputElement>) => {
         if (event.key === 'Enter') {
-            addFn(name)
-            setName("")
+            addItem()
         }
     }
+    const addItem = () => {
+        addFn(name)
+        setName("")
+    }
     return (
-        <TextField value={ name }
-        variant="filled"
-        onChange={ e => setName( e.target.value )}
-        onKeyPress={handleKeyPress}
-        />
+        <Paper className={classes.root}>
+            <InputBase value={name}
+                className={classes.input}
+                onChange={e => setName(e.target.value)}
+                onKeyPress={handleKeyPress}
+            />
+            <Divider className={classes.divider} orientation="vertical"/>
+            <IconButton className={classes.iconButton} onClick={addItem} aria-label="search">
+                <AddIcon />
+            </IconButton>
+        </Paper>
+
     )
 }
